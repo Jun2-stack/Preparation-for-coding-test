8c386429ddfea85a8798a221d70e90e059124fca
HongJun Choi <hjcho1@korea.ac.kr>
2021-03-01 23:26:43 +0900

    Extract only numbers
    숫자만 추출

---
+++ 6.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char input[51] = { 0, };
+	char temp[10] = { 0, };
+	int NN = 0, i = 0, j = 0, count = 0;
+
+	scanf_s("%s", &input, sizeof(input));
+
+	for (i = 0; input[i] !='\0'; i++) // 숫자만 추출
+	{
+		if (input[i] >= 48 && input[i] <= 57)
+		{
+			temp[j] = input[i];
+			j++;
+		}
+	}
+	NN = atoi(temp); // 정수로 변환
+	printf("%d\n", NN);
+
+	for (i = 1; i <= NN; i++) // 약수 구하기
+	{
+		if (NN % i == 0)
+		{
+			count++;
+		}
+	}
+
+	printf("%d", count);
+
+	return 0;
+}
