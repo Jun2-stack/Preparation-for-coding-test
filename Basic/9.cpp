cb7cf33755a721c629cfaafdd51a6b5cbe1a5a64
HongJun Choi <hjcho1@korea.ac.kr>
2021-03-03 16:03:10 +0900

    Update and rename 9.cpp to Basic/9.cpp

--- Basic/9.cpp
+++ Basic/9.cpp
@@ -8,11 +8,11 @@ int main()
 
 	for (i = 1; i<= N; i++)
 	{
-		for(j = 1, count = 0; j <= i && j * j <= i; j++)
+		for(j = 1, count = 0; j <= i && j * j <= i; j++) // 효율적인 약수 구하기
 			if (i % j == 0)
 			{
 				count++;
-				if (j * j < i)
+				if (j * j < i) // 약수의 짝 구하기
 					count++;
 			}
 
